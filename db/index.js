317ce08ca3147288f73018704fd7b0687b8c00ac
Elijah Castillo <elijahcastillo326@yahoo.com>
2021-12-11 14:10:56 -0800

    new fixes

--- db/index.js
+++ db/index.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
-console.log(process.env.JWT_SECRET);
 
 const { Client } = require('pg');
-const usersRouter = require('../api/users');
 
 const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
